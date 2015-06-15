9d2ba5305772741ada579c0aaddfdd852500dc73
Tim Ramsey <tramsey@gmail.com>
2015-06-15 15:28:55 -0400

    Version 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hotpocket "0.2.2"
+(defproject hotpocket "0.2.3-SNAPSHOT"
   :description "A leiningen plugin to add git commit log as a resource file"
   :url "http://github.com/tramzee/hotpocket"
   :license {:name "Eclipse Public License"
